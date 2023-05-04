00dfb3dd66f7e3c3d087d7a7d52c785683a54501
Saugat Singh <saugat.singh09@gmail.com>
2023-05-04 14:37:43 +0545

    prjoect_name = "SnowDepthPrediction"

--- eda.py
+++ eda.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from sys import exit as quit
 import matplotlib.pyplot as plt
+import os 
 
-df = pd.read_csv('predicted_data.csv')
+root_dir = os.getcwd()
+dataset_dir = root_dir+"/dataset/"
+data_file = dataset_dir+"Pluvio_GanjaLa.csv"
+fixed_data_file = dataset_dir+"predicted_data.csv"
+df = pd.read_csv(fixed_data_file)
 
 
 #get df shape information
@@ -17,8 +22,10 @@ print(df.head())
 #check if datas are missing
 print(df.isnull().sum())
 
-# group the data by date
-df_daily = df.groupby('datetime').mean()
+# group the data by date 
+# Date for Original file and datetime for predicted file
+df['Date'] = pd.to_datetime(df['datetime']).dt.date  # for predicted file
+df_daily = df.groupby('Date').mean()
 
 # plot the data
 plt.figure(figsize=(10, 5))
